5fcdf0215e679f2672989dbd87b56ab35110fb38
Brandon Herrera <brandondh769@gmail.com>
2023-06-16 17:23:49 -0600

    Implement features section

--- front/src/pages/home/Home.tsx
+++ front/src/pages/home/Home.tsx
@@ -1,11 +1,14 @@
 import { ReactComponent as Intro } from 'assets/images/Intro.svg';
 import Container from 'react-bootstrap/Container';
+import { Features } from './Features';
+
 
 function Home() {
   return (
     <Container>
       <Intro style={{width: '100%'}}/>
       <h1 style={{ textAlign: 'center', margin: '5%'}}>Features</h1>
+      <Features />
     </Container>
   );
 }
