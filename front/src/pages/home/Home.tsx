3f5479405a5543e5b9301b17a4b42a53d8cdf772
Brandon Herrera <brandondh769@gmail.com>
2023-06-16 15:57:01 -0600

    Add react bootstrap

--- front/src/pages/home/Home.tsx
+++ front/src/pages/home/Home.tsx
@@ -1,9 +1,12 @@
-import { Link } from 'react-router-dom';
 import { ReactComponent as Intro } from 'assets/images/Intro.svg';
+import Container from 'react-bootstrap/Container';
 
 function Home() {
   return (
-    <Intro />
+    <Container>
+      <Intro style={{width: '100%'}}/>
+      <h1 style={{ textAlign: 'center', margin: '5%'}}>Features</h1>
+    </Container>
   );
 }
 
