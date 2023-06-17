b4f85b80a1a63c05175c6bc9cfbfa32ff40fd951
Ricardo Mora <rurimoca@gmail.com>
2023-06-16 23:35:04 -0600

    Match contract

--- back/match-contract/src/lib.rs
+++ back/match-contract/src/lib.rs
@@ -1,14 +1,64 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+#![allow(unused)]
+#![no_std]
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+use gstd::{prelude::*, msg, ActorId};
+use gear_lib::non_fungible_token::token::TokenId;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[derive(Encode, Decode, TypeInfo)]
+pub enum InputMessages {
+    GameFinished {
+        winner: ActorId,
+        winner_card: TokenId,
     }
 }
+
+#[derive(Encode, Decode, TypeInfo)]
+pub enum Answer {
+    FinishSucces
+}
+
+#[derive(Encode, Decode, TypeInfo)]
+pub struct Scoreboard {
+    pub score_p1: u8, 
+    pub score_p2: u8,
+}
+
+#[derive(Encode, Decode, TypeInfo)]
+pub struct GameFinished {
+    pub winner: ActorId, 
+    pub losser: ActorId, 
+    pub game_status: GameState
+}
+
+#[derive(Encode, Decode, TypeInfo)]
+pub enum GameState {
+    Tied,
+    Won
+}
+
+#[derive(Encode, Decode, TypeInfo)]
+pub enum Signvdx {
+    UserId,
+    GameId
+}
+
+#[derive(Encode, Decode, TypeInfo)]
+pub enum TypeId {
+    UserId,
+    GameId
+}
+
+/* #[derive(Encode, Decode, TypeInfo)]
+pub enum TypeId {
+    UserId,
+    GameId
+} */
+
+#[no_mangle]
+extern "C" fn init() {}
+
+#[no_mangle]
+extern "C" fn handle() {}
+
+#[no_mangle]
+extern "C" fn handle_reply() {}
