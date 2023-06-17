00eeacb4060fb71c78e8791742e02726c12fd93e
JuanH <juanh7522@gmail.com>
2023-06-17 10:58:46 -0600

    cards

--- front/src/components/card/Card.tsx
+++ front/src/components/card/Card.tsx
@@ -1,21 +1,30 @@
+import Identicon from '@polkadot/react-identicon';
+import { buttonStyles } from '@gear-js/ui';
 import styles from './Card.module.scss';
+import {Icon} from './icon'
+import {Stars} from './stars'
 
-function Card (){
+
+function Card (props: { image: string; title: string; type: string; value: number; }) {
+    const { image, title, type, value } = props;
+
     return (
     <div className={styles.container}>
         <div className={styles.card}>
-            <img alt="nft" src="/hex.jpg" className={styles.hexagon} />
+            <div className={styles.graphics}>
+                <img className={styles.hexagon} src={image} alt="NFTimage" />
+                <Icon name={type} />
+            </div>
+
 
             <div className={styles.content}>
-                <h2 className={styles.title}>Card</h2>
-                <div>
-                    <p>Title</p>
+                <h3 className={styles.title}>{title}</h3>
+                <div className={styles.typec}>
+                    <p className={styles.type}>{type}</p>
                 </div>
                 <div>
-                    <p>Type</p>
-                </div>
-                <div>
-                    <p>Value</p>
+                    <Stars num={value}/>
+
                 </div>
             </div>
         </div>
--- front/src/pages/profile/Profile.tsx
+++ front/src/pages/profile/Profile.tsx
@@ -1,5 +1,5 @@
 
-
+import Identicon from "@polkadot/react-identicon";
 import './Profile.scss';
 import {Card} from '../../components/card/Card';
 
@@ -7,7 +7,13 @@ function Profile(){
     return(
         <div >
             <h1 className="title">Profile</h1>
-            <Card />
+            <div className="cards">
+                <Card image="/p.jpg" title="SparkPug Blaze" type="fire" value={2}/>
+                <Card image="/pp.jpg" title="SparkPug Blaze" type="water" value={1}/>
+                <Card image="/r.jpg" title="SparkPug Blaze" type="ice" value={3}/>
+                <Card image="/hex.jpg" title="SparkPug Blaze" type="water" value={1}/>
+                <Card image="/d.jpg" title="SparkPug Blaze" type="fire" value={3}/>
+            </div>
 
 
         </div>
