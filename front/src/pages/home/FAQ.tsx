8ae0f8d0639f740d28948f7db39a85e2a27fbe8a
Brandon Herrera <brandondh769@gmail.com>
2023-06-17 10:25:02 -0600

    Add profile logic

--- front/src/pages/home/FAQ.tsx
+++ front/src/pages/home/FAQ.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Accordion from 'react-bootstrap/Accordion';
 import 'bootstrap/dist/css/bootstrap.min.css';
--- front/src/pages/profile/Profile.tsx
+++ front/src/pages/profile/Profile.tsx
@@ -1,17 +1,37 @@
-
-
 import './Profile.scss';
-import {Card} from '../../components/card/Card';
+import { useState } from 'react';
 
-function Profile(){
-    return(
-        <div >
-            <h1 className="title">Profile</h1>
-            
-
-
+function Profile() {
+    const [loggedIn, setLoggedIn] = useState(false); // Estado para controlar si el usuario ha iniciado sesión o no
+  
+    const handleLogin = () => {
+      setLoggedIn(true);
+    };
+  
+    const handleLogout = () => {
+      setLoggedIn(false);
+    };
+  
+const renderProfileDetails = () => {
+    if (loggedIn) {
+      return (
+        <div>
+            <h2>Detalles del usuario</h2>
+            <button type="button" onClick={handleLogout}>Cerrar sesión</button>
         </div>
-    );
-}
+      );
+    }
 
-export { Profile };
+    return (
+      <div>
+        <p>No has iniciado sesión.</p>
+        <button type="button" onClick={handleLogin}>Iniciar sesión</button>
+      </div>
+    );
+  };  
+  
+    return <div className="profile">{renderProfileDetails()}</div>;
+  }
+  
+  export { Profile };
+  
