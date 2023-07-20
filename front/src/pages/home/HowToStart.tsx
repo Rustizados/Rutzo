30cbd9ca19cf72c4bfd7089363d26f63d9ccb183
Brandon Herrera <brandondh769@gmail.com>
2023-07-20 16:55:38 -0600

    Add step 3 in landing page

--- front/src/pages/home/HowToStart.tsx
+++ front/src/pages/home/HowToStart.tsx
@@ -1,6 +1,7 @@
-import { TbHexagonNumber1, TbHexagonNumber2, TbExternalLink } from 'react-icons/tb';
+import { TbHexagonNumber1, TbHexagonNumber2, TbHexagonNumber3, TbExternalLink } from 'react-icons/tb';
 import extension from 'assets/images/howto/extension.png';
 import signin from 'assets/images/howto/signin.png';
+import marketplace from 'assets/images/marketplace.svg';
 import Container from 'react-bootstrap/Container';
 
 function HowToStart() {
@@ -23,6 +24,15 @@ function HowToStart() {
         </h2>
         <p style={{marginBottom: '50px', textAlign: 'center'}}>Select the account to save your NFTs and start playing</p>
         <img src={signin} alt="Select polkadot account" style={{width: '100%'}}/>
+      </Container>
+      <Container style={{ alignSelf: 'center'}}>
+        <h2 style={{ textAlign: 'center', marginBottom: '20px' }}>
+          <TbHexagonNumber3 style={{ fontSize: '300%' }} />
+          <br/><br/>
+          Enter the <a href="/marketplace" className='link-light'>Marketplace   <TbExternalLink /></a>
+        </h2>
+        <p style={{marginBottom: '50px', textAlign: 'center'}}>And get amazing NFTs</p>
+        <img src={marketplace} alt="Marketplace banner" style={{width: '100%'}}/>
       </Container>
     </Container>
     
