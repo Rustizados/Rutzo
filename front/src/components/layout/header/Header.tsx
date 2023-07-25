e4efb4b85a9a06fb433af0e757169583592b357b
Brandon Herrera <brandondh769@gmail.com>
2023-07-24 22:17:02 -0600

    Make nav menu responsive

--- front/src/components/layout/header/Header.tsx
+++ front/src/components/layout/header/Header.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Logo } from './logo';
 import { Account } from './account';
 import styles from './Header.module.scss';
@@ -7,24 +8,21 @@ type Props = {
 };
 
 function Header({ isAccountVisible }: Props) {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
   return (
     <header className={styles.header}>
       <Logo />
-        <nav className={styles.nav}>
-            <ul className={styles.list}>
-                <li><a href="/#features" className='link-light'>FEATURES</a></li>
-                <li><a href="/#how-to-start" className='link-light'>HOW TO START</a></li>
-                <li><a href="/#faq" className='link-light'>FAQ</a></li>
-                <li><a href="/marketplace" className='link-light'>MARKETPLACE</a></li>
-                <li><a href="/profile" className='link-light'>PROFILE</a></li>
-                <li className={styles.highlight}><a href="/play">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={3.5} stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z" />
-                    </svg>
-                    PLAY
-                </a></li>
-            </ul>
-        </nav>
+      <div className={`${styles.nav} ${isMenuOpen ? styles.open : ''}`}>
+        <ul className={`${styles.list} ${isMenuOpen ? styles.show : ''}`}>
+          {/* Menú para pantallas más grandes */}
+          <li><a href="/#features" className='link-light'>FEATURES</a></li>
+          <li><a href="/#how-to-start" className='link-light'>HOW TO START</a></li>
+          <li><a href="/#faq" className='link-light'>FAQ</a></li>
+          <li><a href="/marketplace" className='link-light'>MARKETPLACE</a></li>
+        </ul>
+      </div>
+      <div className={styles.highlight}><a href="/play">PLAY</a></div>
       {isAccountVisible && <Account />}
     </header>
   );
