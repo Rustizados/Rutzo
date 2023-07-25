e884ac4646e41aad797b8de8eee43c12fc6bd7b5
Brandon Herrera <brandondh769@gmail.com>
2023-07-24 22:54:01 -0600

    Make landing page responsive

--- front/src/components/layout/header/Header.tsx
+++ front/src/components/layout/header/Header.tsx
@@ -8,14 +8,13 @@ type Props = {
 };
 
 function Header({ isAccountVisible }: Props) {
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const [isMenuOpen] = React.useState(false);
 
   return (
     <header className={styles.header}>
       <Logo />
       <div className={`${styles.nav} ${isMenuOpen ? styles.open : ''}`}>
         <ul className={`${styles.list} ${isMenuOpen ? styles.show : ''}`}>
-          {/* Menú para pantallas más grandes */}
           <li><a href="/#features" className='link-light'>FEATURES</a></li>
           <li><a href="/#how-to-start" className='link-light'>HOW TO START</a></li>
           <li><a href="/#faq" className='link-light'>FAQ</a></li>
