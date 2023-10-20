fff6ee02dc07d7990383208183e35dae1d5d3011
Brandon Herrera <brandondh769@gmail.com>
2023-10-19 20:45:55 -0600

    Add pages from previous front

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,8 +1,10 @@
 import { Route, Routes } from 'react-router-dom';
+import { Marketplace } from './marketplace';
 import { Home } from './home';
-import { Main } from './main';
+import { Play } from './play';
+import { Profile } from './profile';
 
-const routes = [{ path: '/', Page: Home },{ path: '/main', Page: Main }];
+const routes = [{ path: '/', Page: Home }, { path: '/marketplace', Page: Marketplace }, { path: '/play', Page: Play }, { path: '/profile', Page: Profile }];
 
 function Routing() {
   const getRoutes = () => routes.map(({ path, Page }) => <Route key={path} path={path} element={<Page />} />);
