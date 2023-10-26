c3ad8c04f9282769c4b9089787e724c74ebc68a3
Brandon Herrera <brandondh769@gmail.com>
2023-10-26 09:23:49 -0600

    Update socials

--- front/src/components/layout/footer/socials/Socials.tsx
+++ front/src/components/layout/footer/socials/Socials.tsx
@@ -1,8 +1,12 @@
 import { ReactComponent as Github } from 'assets/images/socials/github.svg';
+import { ReactComponent as X } from 'assets/images/socials/x.svg';
+import { ReactComponent as Instagram } from 'assets/images/socials/instagram.svg';
 import styles from './Socials.module.scss';
 
 const socials = [
-  { href: 'https://github.com/brandonhxrr/Rutzo', icon: Github }
+  { href: 'https://github.com/brandonhxrr/Rutzo', icon: Github },
+  { href: 'https://x.com/Rustizados', icon: X },
+  { href: 'https://instagram.com/rustizados', icon: Instagram }
 ];
 
 function Socials() {
