5c08d4a6a5897e22d37649d2d12e1bf9bd9f4cf0
Brandon Herrera <brandondh769@gmail.com>
2023-10-31 03:11:08 -0600

    Update styles

--- front/src/pages/play/EmptyCollection.tsx
+++ front/src/pages/play/EmptyCollection.tsx
@@ -52,7 +52,7 @@ function EmptyCollection() {
   };
 
   return (
-    <div className="alert">
+    <div className="empty_container">
       <h1>Oops! it looks like you don&apos;t have any cards</h1>
       <img src="https://media.giphy.com/avatars/doodlesbyburnttoast/dMqxHmPPA8fd.gif" alt="empty" id="alert_img" />
       {!isRegistered && (
