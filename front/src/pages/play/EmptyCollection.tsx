502d99afadf24ed2d683476cc2e5e3dbc4f363b0
Brandon Herrera <brandondh769@gmail.com>
2023-10-30 23:36:47 -0600

    Fix card image size

--- front/src/pages/play/EmptyCollection.tsx
+++ front/src/pages/play/EmptyCollection.tsx
@@ -3,7 +3,7 @@ import "./EmptyCollection.scss";
 
 function EmptyCollection() {
   return (
-    <div className="container">
+    <div className="container alert">
       <h1>Oops! it looks like you don&apos;t have any cards</h1>
       <img src="https://media.giphy.com/avatars/doodlesbyburnttoast/dMqxHmPPA8fd.gif" alt="empty"/>
       <p>Register to obtain free cards</p>
