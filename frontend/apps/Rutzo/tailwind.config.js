335111d7f78c7d437806826f749dbe9a7bd00c85
JuanH <juanh7522@gmail.com>
2024-01-29 18:13:17 -0600

    fixing FAQs

--- frontend/apps/Rutzo/tailwind.config.js
+++ frontend/apps/Rutzo/tailwind.config.js
@@ -1,10 +1,23 @@
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+  "./src/**/*.{js,jsx,ts,tsx}",
+  // Incluye otras rutas según sea necesario
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#7834CF',
+        secondary: '#3DDC84',
+        'light-blue': '#89C6FF',
+        'dark-blue': '#3633D0',
+        background: '#080614',
+        'background-gradient': 'linear-gradient(to right, var(--background), #17113A)',
+        gradient: 'linear-gradient(to right, var(--primary-color), var(--secondary-color))',
+        'blue-gradient': 'linear-gradient(to right, var(--light-blue), var(--dark-blue))',
+        acrylic: '#ffffff26',
+      },
+    },
   },
   plugins: [],
 }
