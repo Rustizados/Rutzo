31696567f25e2df1b4e152a8827f2ae4666f4f59
Brandon Herrera <brandondh769@gmail.com>
2024-07-12 22:30:55 -0600

    Improve game results dialog

--- frontend/src/components/board-game/BoardGame2.tsx
+++ frontend/src/components/board-game/BoardGame2.tsx
@@ -80,17 +80,31 @@ function BoardGame2() {
     <div className="flex flex-col items-center justify-center min-h-screen bg-black text-white p-2">
       <div className="w-full max-w-5xl flex justify-between items-center mb-4">
         <div className="flex items-center mt-10">
-          <img src="https://www.rutzo.tech/NFT/lightning/nova_lighting.jpg" alt="Player Avatar" className="w-10 h-10 rounded-full mr-2"/>
-          <p className="text-sm w-96 overflow-hidden whitespace-nowrap truncate">{actualUserInMatch}</p>
+          <img
+            src="https://www.rutzo.tech/NFT/lightning/nova_lighting.jpg"
+            alt="Player Avatar"
+            className="w-10 h-10 rounded-full mr-2"
+          />
+          <p className="text-sm w-96 overflow-hidden whitespace-nowrap truncate">
+            {actualUserInMatch}
+          </p>
         </div>
         <div className="flex items-center">
-          <p className="text-sm w-96 overflow-hidden whitespace-nowrap truncate mr-2">{enemyName}</p>
-          <img src="https://www.rutzo.tech/NFT/poison/angel_of_death_poison.jpg" alt="Enemy Avatar" className="w-10 h-10 rounded-full"/>
+          <p className="text-sm w-96 overflow-hidden whitespace-nowrap truncate mr-2">
+            {enemyName}
+          </p>
+          <img
+            src="https://www.rutzo.tech/NFT/poison/angel_of_death_poison.jpg"
+            alt="Enemy Avatar"
+            className="w-10 h-10 rounded-full"
+          />
         </div>
       </div>
 
       <div className="flex justify-center mb-2 bg-gradient-to-r from-purple-800 to-green-500 rounded-xl">
-        <p className="text-sm p-2 font-bold">{isPlayerTurn ? "Your Turn" : "Enemy's Turn"}</p>
+        <p className="text-sm p-2 font-bold">
+          {isPlayerTurn ? "Your Turn" : "Enemy's Turn"}
+        </p>
       </div>
 
       <div className="w-full max-w-5xl grid grid-cols-2 gap-4 mb-8">
@@ -125,9 +139,9 @@ function BoardGame2() {
               );
             })}
             {selectedCards.length < 3 &&
-              Array.from(Array(3 - selectedCards.length).keys()).map((index) => (
-                <EmptySlot key={`empty-${index}`} />
-              ))}
+              Array.from(Array(3 - selectedCards.length).keys()).map(
+                (index) => <EmptySlot key={`empty-${index}`} />
+              )}
           </div>
         </div>
         <div className="flex flex-col items-center">
@@ -152,50 +166,51 @@ function BoardGame2() {
         </div>
       </div>
 
-      
-
       {userWonTheMatch !== null && (
         <div className="fixed inset-0 flex flex-col items-center justify-center bg-black bg-opacity-75">
-          <div className="bg-white text-black p-8 rounded-lg text-center mb-4">
-            {userWonTheMatch ? <h2>You WON!!</h2> : <h2>You lose :c</h2>}
-          </div>
-          <div className="flex space-x-4">
-            <button
-              onClick={handleNewGame}
-              className="px-6 py-2 bg-gradient-to-r from-purple-800 to-green-500 text-white rounded-full shadow-md hover:shadow-lg"
-            >
-              New Game
-            </button>
-            <button
-              onClick={handleGoHome}
-              className="px-6 py-2 bg-gradient-to-r from-purple-800 to-green-500 text-white rounded-full shadow-md hover:shadow-lg"
-            >
-              Home
-            </button>
+          <div className="bg-black text-white p-8 rounded-lg text-center mb-4 border-2 border-violet-800">
+            {userWonTheMatch ? (
+              <h2 className="text-lg">You won! 🎉</h2>
+            ) : (
+              <h2>You lose ):</h2>
+            )}
+            <div className="flex space-x-4 mt-8">
+              <button
+                onClick={handleNewGame}
+                className="px-6 py-2 bg-gradient-to-r from-purple-800 to-green-500 text-white rounded-full shadow-md hover:shadow-lg"
+              >
+                New Game
+              </button>
+              <button
+                onClick={handleGoHome}
+                className="px-6 py-2 bg-gradient-to-r from-purple-800 to-green-500 text-white rounded-full shadow-md hover:shadow-lg"
+              >
+                Home
+              </button>
+            </div>
           </div>
         </div>
       )}
 
-{showDialog && (
-  <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75">
-    <div className="bg-black text-white p-8 rounded-lg text-center  border-2 border-violet-800">
-      <h2 className="text-lg mb-3">Searching for an opponent...</h2>
-      <p>
-        Time left: {Math.floor(timeLeft / 60)}:
-        {(timeLeft % 60).toString().padStart(2, '0')}
-      </p>
-      <div className="flex justify-center mt-8">
-        <button
-          onClick={cancelMatch}
-          className="px-6 py-2 bg-gradient-to-r from-red-800 to-red-500 text-white rounded-full shadow-md hover:shadow-lg"
-        >
-          Cancel Match
-        </button>
-      </div>
-    </div>
-  </div>
-)}
-
+      {showDialog && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75">
+          <div className="bg-black text-white p-8 rounded-lg text-center border-2 border-violet-800">
+            <h2 className="text-lg mb-3">Searching for an opponent...</h2>
+            <p>
+              Time left: {Math.floor(timeLeft / 60)}:
+              {(timeLeft % 60).toString().padStart(2, "0")}
+            </p>
+            <div className="flex justify-center mt-8">
+              <button
+                onClick={cancelMatch}
+                className="px-6 py-2 bg-gradient-to-r from-red-800 to-red-500 text-white rounded-full shadow-md hover:shadow-lg"
+              >
+                Cancel Match
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
 
       <div className="fixed bottom-4 right-4">
         <button
